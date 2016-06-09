50e313b96f34b4020f35d7542b589012ca34e3d6
kellymcguffog <KellyMcGuffog@Kellys-MacBook-Pro.local>
2016-06-09 12:17:47 +0200

    day8 folder

---
+++ Day3/day7afternoon.js
@@ -0,0 +1,38 @@
+// change the color of the text in the div with class "color-div" to red
+function changeColor() {
+    $("color-div").css("color", "red;");
+
+// add the word "blue" to the div with class "add-div"
+function addToDiv() {
+	
+}
+
+// add a title to the page
+function addTitle() {
+	
+}
+
+// alert the text in the selected item of the dropdown
+function getSelected() {
+	
+}
+
+// change the title to be "New Title"
+function changeText() {
+
+}
+
+$(document).ready(function() {
+
+  $('.the-button').click(function(){ 
+    //hint: what goes in here?
+	});
+	
+
+
+  $(".trio").click(function (e) {
+    //hint: look at the currentTarget property
+  });
+
+});
+
